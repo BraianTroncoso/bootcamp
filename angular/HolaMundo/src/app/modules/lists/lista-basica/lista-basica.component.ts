96dd92cbb93ab63448f80ac976f7baaaf3bb0ce3
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-05-02 17:08:04 -0300

    [ADD] Clase 4 - Directivas - Completa

--- angular/HolaMundo/src/app/modules/lists/lista-basica/lista-basica.component.ts
+++ angular/HolaMundo/src/app/modules/lists/lista-basica/lista-basica.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+// Creamos un tipo base para el ejemplo
+export type Producto = {
+  nombre: string;
+  precio: number;
+  descripcion: string
+}
 @Component({
   selector: 'app-lista-basica',
   templateUrl: './lista-basica.component.html',
   styleUrls: ['./lista-basica.component.scss']
 })
-export class ListaBasicaComponent {
+export class ListaBasicaComponent implements OnInit{
 
+  listaElementos: Producto[] = [
+    {
+      nombre:'Leche',
+      precio: 10,
+      descripcion: 'Leche entera'
+    },
+    {
+      nombre:'Carne',
+      precio: 20,
+      descripcion: 'Carne de cerdo'
+    }
+  ];
+  cargando: boolean = true;  
+
+  opcion: number = 0;
+
+  constructor() {}
+
+ngOnInit(): void {
+  
+}
+
+cambiarCargando(){
+  this.cargando = !this.cargando;
+}
+escogerOpcion(opcionEscogida: number){
+  this.opcion = opcionEscogida; // Dado que el valor va a cambiar, implica un cambio en los elementos a renderizar
+}
 }
